4ea3589cbe6a45af72700709dced426f278d1594
Rudenko Artem <avenight218@gmail.com>
2026-05-10 17:36:16 +0300

    First commit

---
+++ src/test/java/ua/rud/teammanagementsystem/TeamManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.rud.teammanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
