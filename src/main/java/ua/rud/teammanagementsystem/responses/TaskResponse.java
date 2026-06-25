14daecdf4f59b0d5ceaa44c84b18f5ba340ded33
Rudenko Artem <avenight218@gmail.com>
2026-06-25 15:01:47 +0300

    Fixed typo

--- src/main/java/ua/rud/teammanagementsystem/responses/TaskResponse.java
+++ src/main/java/ua/rud/teammanagementsystem/responses/TaskResponse.java
@@ -5,5 +5,5 @@ import ua.rud.teammanagementsystem.enums.TaskStatus;
 
 import java.time.LocalDate;
 
-public record TaskResponse(Long id, String tittle, String description, TaskStatus status, TaskPriority priority, LocalDate deadline, Long project_id, Long user_id) {
+public record TaskResponse(Long id, String title, String description, TaskStatus status, TaskPriority priority, LocalDate deadline, Long project_id, Long user_id) {
 }
